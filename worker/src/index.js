bbf0644872e062efa19efefb7ac13e39a69caabe
egobeznik <egobeznik@gmail.com>
2026-08-18 19:35:12 +0300

    Add Worker API

---
+++ worker/src/index.js
@@ -0,0 +1,16 @@
+export default {
+    async fetch(request) {
+        return new Response(
+            JSON.stringify({
+                success: true,
+                service: "poselenie-api",
+                status: "online"
+            }),
+            {
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8"
+                }
+            }
+        );
+    }
+};
